61b61ccf8c373e0bf395be861b4ace7e2be7cf44
yachikajoshi <42508829+yachikajoshi@users.noreply.github.com>
2026-04-29 00:10:27 +0530

    Add: two-integer-sum - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum/submission-8.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun twoSum(arr: IntArray, target: Int): IntArray {
+        var hashMap = mutableMapOf<Int, Int>()
+       
+        for ((index, num) in arr.withIndex()) {
+            val diff = target - num
+            if (hashMap.containsKey(diff)) {
+               return intArrayOf(hashMap.getValue(diff), index)
+            }
+            hashMap[num] = index
+        }
+   return intArrayOf(0)
+    }
+}
