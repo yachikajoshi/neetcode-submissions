a8cc5641d1b554584f7ad6607956b0b6ab8290bc
yachikajoshi <42508829+yachikajoshi@users.noreply.github.com>
2026-05-04 17:56:44 +0530

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.kt
@@ -0,0 +1,27 @@
+class Solution {
+  fun isPalindrome(s: String): Boolean {
+    val newString = s.replace(" ","")
+    var left = 0
+    var right = newString.length - 1
+    while (left < right) {
+          while (left < right && !isAlphaNumericChar(newString[left])) {
+            left++
+        }
+          while (left < right && !isAlphaNumericChar(newString[right])) {
+            right--
+        }
+        if (newString[left].lowercaseChar() != newString[right].lowercaseChar()) {
+            return false
+        }
+        left++
+        right--
+    }
+
+    return true
+}
+
+fun isAlphaNumericChar(char: Char): Boolean {
+    val regex = Regex("[A-Za-z0-9]")
+    return regex.matches(char.toString())
+}
+}
