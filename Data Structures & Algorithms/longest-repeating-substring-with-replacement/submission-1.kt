9955cfd666a39b23909fcb3b5ad1cc1e5f1dc534
yachikajoshi <42508829+yachikajoshi@users.noreply.github.com>
2026-05-25 19:28:50 +0530

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.kt
@@ -0,0 +1,25 @@
+class Solution {
+    fun characterReplacement(s: String, k: Int): Int {
+        val count = mutableMapOf<Char, Int>()
+        var result = 0
+        var l = 0
+        var maxFrequentNum = 0
+        for(r in s.indices){
+            count[s[r]] = count.getOrDefault(s[r],0)+1
+            maxFrequentNum = maxOf(maxFrequentNum, count[s[r]]!!)
+            while (r-l+1 - maxFrequentNum > k){
+                count[s[l]] = count[s[l]]!! - 1
+                l++
+            }
+            result = maxOf(result, r - l + 1)
+        }
+        return result
+    }
+     fun maxOf(left: Int, right: Int): Int{
+        if(left < right){
+            return right
+        } else {
+            return left
+        }
+    }
+}
